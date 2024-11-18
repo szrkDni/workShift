da9d2bb50f5cd16fb3a7983a77750a6dd8522670
dániel szarka <szarka.danika@gmail.com>
2024-11-18 19:50:01 +0100

    password handling

---
+++ WorkShiftApp/src/main/java/com/project/springjavafx/javaFXApp/utility/PasswordHandler.java
@@ -0,0 +1,25 @@
+package com.project.springjavafx.javaFXApp.utility;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class PasswordHandler {
+
+    public static String encrypt(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hashBytes);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+    }
+
+    public static boolean verify(String password, String hash) {
+
+        return encrypt(password).equals(hash);
+    }
+}
